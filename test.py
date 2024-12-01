4a0351549da2cb85c41a7996e92b7c515ddb3679
CALLIA <alexsandrralexsandr@gmail.com>
2024-12-01 19:16:15 +0300

    test change

--- test.py
+++ test.py
@@ -1,23 +1,10 @@
-def count_cells(a, b, c, k):
-    black_cells = a * a
-    red_cells = 0
-    current_side = a
-
-    for i in range(1, k + 1):
-        if i % 2 == 1:
-            current_side += 2 * b
-            new_red_cells = current_side * current_side - \
-                (current_side - 2 * b) * (current_side - 2 * b)
-            red_cells += new_red_cells
-        else:
-            current_side += 2 * c
-            new_black_cells = current_side * current_side - \
-                (current_side - 2 * c) * (current_side - 2 * c)
-            black_cells += new_black_cells
-
-    return black_cells, red_cells
-
-
-a, b, c, k = map(int, input().strip().split())
-black_cells, red_cells = count_cells(a, b, c, k)
-print(black_cells, red_cells)
+q = int(input())
+box = []
+for i in range(q):
+    cmd = input().strip().split()
+    if cmd[0] == "push":
+        box.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        box.pop()
+    elif cmd[0] == 'max':
+        print(max(box))

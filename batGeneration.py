cda6cde1937f2b043258973ec9d90521cc24ba08
CALLIA <alexsandrralexsandr@gmail.com>
2024-12-21 23:38:11 +0300

    Only Data

--- batGeneration.py
+++
@@ -1,6 +0,0 @@
-PATH_ME = __file__[:len(__file__) - len('batGeneration.py')]
-
-with open('Helper.bat', mode="w+") as f:
-    f.write(f'''python "{PATH_ME}starter.py"''')
-
-print('Создан файл Helper.bat активируйте, для запуска ассистента)')
